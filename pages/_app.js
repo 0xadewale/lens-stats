f6042c7fc30a02fb877243c1d1b61382266746f1
0xadewale <crypto.adewale@gmail.com>
2022-09-05 16:45:38 +0200

    wip

--- pages/_app.js
+++ pages/_app.js
@@ -106,16 +106,19 @@ function MyApp({ Component, pageProps }) {
                 signIn={signIn}
             />
             <Box pt={4} pb='10rem'>
-              <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
+              <Script
+                  strategy="lazyOnload"
+                  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+              />
               <Script id="google-analytics" strategy="lazyOnload">
                 {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                  page_path: window.location.pathname,
-                });
-              `}
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
               </Script>
               <Component {...pageProps} signIn={signIn} />
             </Box>
