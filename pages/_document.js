8d171e33dcb0c1983a72a846813dd12a203728ae
0xadewale <crypto.adewale@gmail.com>
2022-09-05 16:31:38 +0200

    analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,12 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import theme from '../theme'
-import Script from 'next/script'
 
 export default class _document extends NextDocument {
   render() {
     return (
         <Html lang='en'>
-          <Head>
-            <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}></Script>
-            <Script id="google-analytics" strategy="lazyOnload">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `}
-            </Script>
-          </Head>
+          <Head />
           <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
