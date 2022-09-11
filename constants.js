4c95be726e32de6372189d198133d85f508b6b82
Thomas <thomas@pop-os.localdomain>
2022-09-11 15:55:04 +0200

    added logo

--- constants.js
+++ constants.js
@@ -2,4 +2,4 @@
 export const APP_NAME = 'Lenstats';
 export const DESCRIPTION = 'Lenstats is web app to track data on Lens Protocol 🌿';
 
-export const DEFAULT_OG = 'https://lenstats.xyz/icon.svg';
+export const DEFAULT_OG = 'https://lenstats.xyz/icon-full.webp';
